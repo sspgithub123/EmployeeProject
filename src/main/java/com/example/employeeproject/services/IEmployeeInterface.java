3997c733abe535cf0cb21de37800faabcf6d94a1
Shubham <ssp671995@gmail.com>
2022-05-23 09:00:36 +0530

    Added the day 7 assignment in the program.

--- src/main/java/com/example/employeeproject/services/IEmployeeInterface.java
+++ src/main/java/com/example/employeeproject/services/IEmployeeInterface.java
@@ -1,16 +1,18 @@
 package com.example.employeeproject.services;
 
+
 import com.example.employeeproject.dto.EmployeeDTO;
 import com.example.employeeproject.module.Employee;
 
 import java.util.List;
-
 
 public interface IEmployeeInterface{
 
     String removeById(int id);
 
     String editById(int id, EmployeeDTO employee);
+
+    List<Employee> getEmployeeByDepartment(String department);
 
     List<Employee> searchAll();
 
